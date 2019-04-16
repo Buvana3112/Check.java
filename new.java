c4ae65ab4f899840a7de0af1148cdcac9bd857e3
Buvana3112 <40532123+Buvana3112@users.noreply.github.com>
2019-04-16 16:46:01 +0530

    Create new.java

---
+++ new.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class new{
+     public static void main(String args[]){
+           Scanner s=new Scanner(Systems.in);
+           int a=s.nextInt();
+           int b=s.nextInt();
+           int c=1;
+           while(b!=0){
+              c=c*a;
+              b=b-1;
+           }
+           System.out.println(c);
+    }
+}
+              
